0ea417f365ff81c7db6370ff737f7a2d07e03a1b
kauaferr <qnobi0403@gmail.com>
2022-10-05 22:10:37 -0300

    adicionando arquivos

--- src/App/services/TarefasService.js
+++ src/App/services/TarefasService.js
@@ -1,68 +1,40 @@
 import ApiService from "../apiservice";
 
 
-class TarefasService extends ApiService{
-    constructor(){
+class TarefasService extends ApiService {
+    constructor() {
         super('/api/tarefas')
     }
 
-    obterListaMes(){
-        const lista = [
-            { label: 'Selecione', valor: '' },
-            { label: 'Janeiro', valor: 1 },
-            { label: 'Fevereiro', valor: 2 },
-            { label: 'Março', valor: 3 },
-            { label: 'Abril', valor: 4 },
-            { label: 'Maio', valor: 5 },
-            { label: 'Junho', valor: 6 },
-            { label: 'Julho', valor: 7 },
-            { label: 'Agosto', valor: 8 },
-            { label: 'Setembro', valor: 9 },
-            { label: 'Outubro', valor: 10 },
-            { label: 'Novembro', valor: 11 },
-            { label: 'Dezembro', valor: 12 },
-        ]
-
-        return lista
-    }
-
-    obterListaTipo(){
-        return [
-            { label: 'Selecione', valor: '' },
-            { label: 'Despesa', valor: 'DESPESA' },
-            { label: 'Receita', valor: 'RECEITA' },
-        ]
-    }
-
-    salvar(objeto){
+    salvar(objeto) {
         return this.post('/', objeto);
     }
-    deletar(id){
+    deletar(id) {
         return this.delete(`/${id}`)
     }
 
-    editar(id, objeto){
-        return this.put(`?id=${id}`, objeto )
+    editar(id, objeto) {
+        return this.put(`?id=${id}`, objeto)
     }
 
-    consultar(objeto){
+    consultar(objeto) {
 
         let params = `?usuario=${objeto.usuario}`
 
-        if(objeto.descricao){
-            params= `${params}&descricao=${objeto.descricao}`
+        if (objeto.descricao) {
+            params = `${params}&descricao=${objeto.descricao}`
 
         }
 
-        
-        if(objeto.concluido != null){
-            params= `${params}&concluido=${objeto.concluido}`
+
+        if (objeto.concluido != null) {
+            params = `${params}&concluido=${objeto.concluido}`
 
         }
         console.log(params)
         return this.get(params)
     }
 
-    
+
 }
 export default TarefasService;
